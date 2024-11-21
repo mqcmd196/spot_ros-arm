4347e5533080bee80850e2e85bbdfe59878ba4c6
Kei Okada <kei.okada@gmail.com>
2024-11-21 11:46:39 +0900

    spot_driver/setup.py: add spot_driver.arm to packages to install modules

--- spot_driver/setup.py
+++ spot_driver/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['spot_driver'],
+    packages=['spot_driver', 'spot_driver.arm', 'spot_driver.arm.arm_utilities'],
     scripts=['scripts/spot_ros'],
     package_dir={'': 'src'}
 )
